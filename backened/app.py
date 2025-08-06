f9390b0b86d6249d2304fd1e13d23e0aade590f8
shruti8766 <shrutigaikwad8766>
2025-08-06 10:19:51 +0530

    Your descriptive commit message here

--- backened/app.py
+++ backened/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request  # Added request import
 from flask_cors import CORS
 import dbHelper
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -29,4 +30,5 @@ def leaderboard():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
